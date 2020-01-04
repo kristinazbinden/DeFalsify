29b2d3bb4e9e51dbda38979b971983e39bec8eb7
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2020-01-03 20:08:25 -0800

    creating file tree structure

--- index.php
+++ index.php
@@ -1,10 +1,68 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="app/assets/styles/style.css"/>
         <meta charset="utf-8">
         <title></title>
     </head>
     <body>
-        hi.
+
+    <div class="site-wrapper">
+
+        <div class="site-wrapper-inner">
+
+            <div class="cover-container">
+
+                <div class="masthead clearfix">
+                    <div class="inner">
+                        <h3 class="masthead-brand">AngularJS's Portfolio</h3>
+                        <nav class="nav nav-masthead">
+
+                        <!-- NAVIGATION_PLACEHOLDER -->
+                        </nav>
+                    </div>
+                </div>
+
+                <div class="inner cover">
+
+                    <!-- UI_VIEW_PLACEHOLDER -->
+
+
+                </div>
+
+                <div class="mastfoot">
+                    <div class="inner">
+                        <p>Cover template for
+                            <a href="https://getbootstrap.com">Bootstrap</a>, by
+                            <a href="https://twitter.com/mdo">@mdo</a>.</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <ul>
+
+
+
+
+
+    </ul>
+
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
+        crossorigin="anonymous"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.6/angular.min.js"></script>
+    <script src="https://unpkg.com/@uirouter/angularjs/release/angular-ui-router.js"></script>
+    <script src="app/app.js"></script>    
     </body>
 </html>
