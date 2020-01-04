55a26fd5ba186fcb01d797027bc58c0a0aa3b8a7
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2020-01-03 17:59:43 -0800

    Deleted backend files

--- DeFalsify-Frontend/app.js
+++ DeFalsify-Frontend/app.js
@@ -1,5 +1,9 @@
 const app = angular.module('DeFalsify', []);
 
+
 app.controller('MainController', function(){
-    this.hello = 'oh hai!';
+    this.factcheckURL = 'https://factchecktools.googleapis.com/v1alpha1/claims:search';
+    this.apikey = 'apikey=' + 'AIzaSyDeCqZ5sqc0YjcHlMiS1Q7MG6oY9WZ8y7U';
+    this.query = 
+
 })
