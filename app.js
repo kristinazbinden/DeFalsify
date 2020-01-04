568a1d1b3144295bab56d46fea9f908ed0586dcd
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2020-01-03 19:32:54 -0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const app = angular.module('DeFalsify', []);
-
-
-app.controller('MainController', function(){
-    this.factcheckURL = 'https://factchecktools.googleapis.com/v1alpha1/claims:search';
-    this.apikey = 'apikey=' + 'AIzaSyDeCqZ5sqc0YjcHlMiS1Q7MG6oY9WZ8y7U';
-    this.query = 
-
-})
