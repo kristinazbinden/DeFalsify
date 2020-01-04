f74864810dfaec21dca6dced4f72cff626ffbb63
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2020-01-03 19:51:51 -0800

    make it better

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    echo 'In the words of the venerable Gwen Stefani - "Ya\'ll  still don\'t know what a Hollaback girl is, do you?" *spits* ';
+?>
--- localAPI/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo 'In the words of the venerable Gwen Stefani - "Ya\'ll nutsacks still don\'t know what a Hollaback girl is, do you?" *spits* ';
-?>
