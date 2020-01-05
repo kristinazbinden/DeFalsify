c827478c18f4a87a94616434b8d33be254670cc7
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2020-01-04 23:28:04 -0800

    starting fresh with node.js server

---
+++ server.js
@@ -0,0 +1,35 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+const session = require('express-session');
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+/**
+ *  App Configuration
+ */
+app.use(express.json());
+app.use(express.static('public'));
+app.use(session({
+    secret:'feedmeseymour',
+    resave:false,
+    saveUninitialized:false
+}))
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+    res.status(200).send("I love shmaghetti")
+})
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+    console.log('Listening to requests on http://localhost:8000');
+})
