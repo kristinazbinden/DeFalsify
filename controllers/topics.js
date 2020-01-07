568b3367edb2642419e35aea8c2cc3b995f38a37
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2020-01-07 13:14:23 -0800

    added styling

--- controllers/topics.js
+++ controllers/topics.js
@@ -32,7 +32,7 @@ router.delete("/:id", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-    Topic.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updateTopic) => {
+    Topic.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedTopic) => {
         res.json(updatedTopic)
     })
 });
